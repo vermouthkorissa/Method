9188ff86c72ecc5a8a43b7f6b04ce5f711ea90a3
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-06-23 19:44:49 +0900

    dekita

--- Method/ViewController.swift
+++ Method/ViewController.swift
@@ -9,10 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var goukeiLabel: UILabel!
+    
+    @IBOutlet weak var maeTextField: UITextField!
+    @IBOutlet weak var usiTextField: UITextField!
+    
+    var number1 : Int = 0
+    var number2 : Int = 0
+    var number3 : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+            guard let number1 = Int(maeTextField.text ?? ""),
+                  let number2 = Int(usiTextField.text ?? "") else {
+                goukeiLabel.text = "Invalid input"
+                return
+            }
+            number3 = number1 + number2
+            goukeiLabel.text = String(number3)
+        }
+
+        func addNumbers(_ num1: Int, _ num2: Int) -> Int {
+            return num1 + num2
+        }
 
 
 }
